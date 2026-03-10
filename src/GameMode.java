6a027993e39798ab9e90a80fcd5d39fb382f8e25
Jennifer Gentry <csprofgentry@gmail.com>
2026-03-10 09:29:15 -0400

    Add GameMode class with mode name and display method

--- src/GameMode.java
+++ src/GameMode.java
@@ -1,1 +1,11 @@
+public class GameMode {
+    private String modeName;
 
+    public GameMode(String modeName) {
+        this.modeName = modeName;
+    }
+
+    public void displayMode() {
+        System.out.println("Current game mode: " + modeName);
+    }
+}
